65f9a91106f115e3855ce6566a23c92c5c84d375
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-01 22:22:47 +0530

    -check if sorted

---
+++ Day 3/CheckIfSorted.java
@@ -0,0 +1,12 @@
+public class CheckIfSorted {
+    public static int isSorted(int n, int[] a) {
+        // Write your code here.
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+}
