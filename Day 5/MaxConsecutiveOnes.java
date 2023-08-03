9e90e0d8b8b4fb47b34154aeecdbf0ee83e26ca6
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-03 10:53:56 +0530

    -max consecutive ones

---
+++ Day 5/MaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int cnt = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cnt++;
+                max = Math.max(max, cnt);
+            } else {
+                cnt = 0;
+            }
+        }
+        return max;
+    }
+}
