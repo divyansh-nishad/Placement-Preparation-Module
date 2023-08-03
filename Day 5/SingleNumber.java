ecc0b0d2a8dd69b1a3c45f972b941d783359cc11
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-03 11:08:14 +0530

    -single number using xor

--- Day 5/SingleNumber.java
+++ Day 5/SingleNumber.java
@@ -1,5 +1,10 @@
 public class SingleNumber {
-    
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++)
+            xor^=nums[i];
+        return xor;
+    }
     
 }
 class Solution {
